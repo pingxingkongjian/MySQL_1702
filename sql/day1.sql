04733f6f3b8c9f5616a580129723c7b2df831a02
pingxingkongjian <153248967@qq.com>
2017-05-15 17:38:17 +0800

    Update

--- sql/day1.sql
+++ sql/day1.sql
@@ -1,1 +1,22 @@
 SHOW DATABASES ;
+
+CREATE DATABASE db_1702;
+
+DROP DATABASE db_1702;
+
+USE db_1702;
+
+SHOW TABLES ;
+
+CREATE TABLE t_student(
+  sno VARCHAR(10),
+  sname VARCHAR(10),
+  gender CHAR(1),
+  age INT(2)
+);
+
+SELECT *
+FROM t_student;
+
+INSERT INTO t_student
+    VALUE ('2017001','张三','男',18);
